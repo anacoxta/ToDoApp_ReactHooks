6113195f9afe4afa3c86c578b665539073611f75
anacoxta <ana.costa.eduardo@gmail.com>
2019-05-05 09:31:04 -0300

    Starting point for app and some css

---
+++ src/TasksApp.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+
+function TasksApp() {
+  return (
+    <main className="app">
+      <header className="app-header">
+        <h1>Task Manager</h1>
+      </header>
+
+      <ul>
+        <li>bla</li>
+        <li>bla</li>
+      </ul>
+    </main>
+  )
+}
+
+export default TasksApp
